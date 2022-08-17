95d8751029ea8d6c9699c33859024ef7698a9711
MINGYU KIM <84526709+k0136000@users.noreply.github.com>
2022-08-18 01:49:48 +0900

    Delete src directory

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import app from "./app";
-
-app.listen(80, () => {
-  console.log("Server open at 80");
-});
--- src/serverless.js
+++
@@ -1,4 +0,0 @@
-import serverless from "serverless-http";
-import app from "./app";
-
-exports.handler = serverless(app);
